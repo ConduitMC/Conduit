000b4c1d3dbfd767321db9e58ed53c98966afe6c
Innectic <innectic@gmail.com>
2019-10-02 21:48:06 -0700

    Begin on block event mixins

---
+++ src/main/java/me/ifydev/serverwrapper/events/BlockEventMixin.java
@@ -0,0 +1,32 @@
+package me.ifydev.serverwrapper.events;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.protocol.game.ClientboundBlockBreakAckPacket;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+/**
+ * @author Innectic
+ * @since 10/2/2019
+ */
+@Mixin(value = net.minecraft.server.level.ServerPlayerGameMode.class, remap = false)
+public class BlockEventMixin {
+
+    @Shadow private ServerLevel level;
+    @Shadow private ServerPlayer player;
+
+    /**
+     * @author Innectic
+     */
+    @Overwrite
+    public boolean destroyBlock(BlockPos pos) {
+        BlockState block = this.level.getBlockState(pos);
+
+        System.out.println("Block at " + pos.getX() + ", " + pos.getY() + ", " + pos.getZ() + " has been broken!");
+        return true;
+    }
+}

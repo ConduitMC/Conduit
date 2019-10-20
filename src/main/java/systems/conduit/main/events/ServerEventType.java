45ece9d67b098bdff5c435a00d1a2b92f6b8e077
Innectic <innectic@gmail.com>
2019-10-19 22:18:31 -0700

    cleanup: remove dev code

---
+++ src/main/java/systems/conduit/main/events/ServerEventType.java
@@ -0,0 +1,18 @@
+package systems.conduit.main.events;
+
+/**
+ * @author Innectic
+ * @since 10/19/2019
+ */
+public class ServerEventType {
+
+    public static class ServerInitializeEvent extends EventType {
+    }
+
+    public static class ServerStartedEvent extends EventType {
+    }
+
+    public static class ServerLoadingWorldsEvent extends EventType {
+
+    }
+}

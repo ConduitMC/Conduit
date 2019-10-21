a9ecebb7adf89706f2a1e6ea4ca64405a1c7d553
Innectic <innectic@gmail.com>
2019-10-21 00:56:52 -0700

    feat: add new world event types

--- src/main/java/systems/conduit/main/events/types/WorldEvents.java
+++ src/main/java/systems/conduit/main/events/types/WorldEvents.java
@@ -3,11 +3,14 @@ package systems.conduit.main.events.types;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import net.minecraft.core.Direction;
+import net.minecraft.core.Location;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.block.state.BlockState;
 import systems.conduit.main.api.Player;
 import systems.conduit.main.events.Cancellable;
+import systems.conduit.main.events.EventType;
 
 /**
  * @author Innectic
@@ -41,5 +44,46 @@ public class WorldEvents {
     public static class BlockBreakEvent extends Cancellable {
         private Player player;
         private BlockState blockState;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class WorldSaveEvent extends EventType {
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class ChunkLoadEvent extends Cancellable {
+        private ChunkPos chunk;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class ChunkUnloadEvent extends Cancellable {
+        private ChunkPos chunk;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class PortalCreateEvent extends EventType {
+        // TODO: Whatever goes in here
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class WeatherChangeEvent extends Cancellable {
+        // TODO: Whatever goes in here
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class LightningStrikeEvent extends Cancellable {
+        private Location location;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class ThunderStatusChangeEvent extends Cancellable {
+        private boolean isThundering;
     }
 }

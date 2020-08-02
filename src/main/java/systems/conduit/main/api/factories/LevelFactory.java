b418c67595d98f5fe821ffe52563abb0cb03fa3e
Innectic <innectic@gmail.com>
2020-08-01 20:52:16 -0700

    feat: add params for world gen

--- src/main/java/systems/conduit/main/api/factories/LevelFactory.java
+++ src/main/java/systems/conduit/main/api/factories/LevelFactory.java
@@ -1,6 +1,11 @@
 package systems.conduit.main.api.factories;
 
 import lombok.Builder;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.level.DataPackConfig;
+import net.minecraft.world.level.GameRules;
+import net.minecraft.world.level.GameType;
+import net.minecraft.world.level.LevelSettings;
 
 /**
  * Handles the creation of new levels.
@@ -10,11 +15,32 @@ import lombok.Builder;
  */
 @Builder
 public class LevelFactory {
-    private String name;
+    private String levelName;
+    private GameType gameType;
+    private boolean hardcore;
+    private Difficulty difficulty;
+    private boolean allowCommands;
+    private GameRules gameRules;
+    private DataPackConfig dataPackConfig;
 
-    // TODO
+    private long fixedTime;
+    private boolean hasSkylight;
+    private boolean hasCeiling;
+    private boolean ultraWarm;
+    private boolean natural;
+    private boolean shrunk;
+    private boolean createDragonFight;
+    private boolean piglinSafe;
+    private boolean bedWorks;
+    private boolean respawnAnchorWorks;
+    private boolean hasRaids;
+    private int logicalHeight;
+    private float ambientLight;
 
-    public void create() {
+    private long seed;
+
+    public void generate() {
+        LevelSettings settings = new LevelSettings(levelName, gameType, hardcore, difficulty, allowCommands, gameRules, dataPackConfig);
         throw new UnsupportedOperationException("not implemented");
     }
 }

38ae018a31a0099ed0facb23684065816dfd1b21
Innectic <innectic@gmail.com>
2021-01-05 23:20:23 -0800

    feat: add scoreboard manager

---
+++ src/main/java/systems/conduit/main/core/scoreboard/ScoreboardManager.java
@@ -0,0 +1,65 @@
+package systems.conduit.main.core.scoreboard;
+
+import net.minecraft.world.scores.Objective;
+import net.minecraft.world.scores.PlayerTeam;
+import systems.conduit.main.Conduit;
+import systems.conduit.main.api.mixins.ServerPlayer;
+
+import java.util.Optional;
+
+/**
+ * @author Innectic
+ * @since 1/3/2021
+ */
+public class ScoreboardManager {
+
+    public Optional<Objective> getOrCreateObjective(String name) {
+        return Conduit.getServer().map(s -> s.getScoreboard().getOrCreateObjective(name));
+    }
+
+    public boolean isExistingObjective(String name) {
+        return Conduit.getServer().map(s -> s.getScoreboard().getObjective(name) == null).orElse(false);
+    }
+
+    public void deleteObjective(String name) {
+        if (!isExistingObjective(name)) return;
+
+        Conduit.getServer().ifPresent(s -> s.getScoreboard().removeObjective(s.getScoreboard().getObjective(name)));
+    }
+
+    public Optional<PlayerTeam> getTeam(String name) {
+        return Conduit.getServer().map(s -> s.getScoreboard().getPlayerTeam(name));
+    }
+
+    public boolean teamExists(String name) {
+        return getTeam(name).isPresent();
+    }
+
+    public Optional<PlayerTeam> getTeamForPlayer(ServerPlayer player) {
+        return getTeamForPlayer(player.getGameProfile().getName());
+    }
+
+    public Optional<PlayerTeam> getTeamForPlayer(String player) {
+        return Conduit.getServer().map(s -> s.getScoreboard().getPlayersTeam(player));
+    }
+
+    public void addPlayerToTeam(ServerPlayer player, String team) {
+        addPlayerToTeam(player.getGameProfile().getName(), team);
+    }
+
+    public void addPlayerToTeam(String player, String name) {
+        Optional<PlayerTeam> team = getTeam(name);
+        if (!team.isPresent()) return;
+        Conduit.getServer().ifPresent(s -> s.getScoreboard().addPlayerToTeam(player, team.get()));
+    }
+
+    public void removePlayerFromTeam(ServerPlayer player, String team) {
+        removePlayerFromTeam(player.getGameProfile().getName(), team);
+    }
+
+    public void removePlayerFromTeam(String player, String name) {
+        Optional<PlayerTeam> team = getTeam(name);
+        if (!team.isPresent()) return;
+        Conduit.getServer().ifPresent(s -> s.getScoreboard().removePlayerFromTeam(player, team.get()));
+    }
+}
